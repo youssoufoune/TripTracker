3906ed5a8e6b854a1376604e9a6b7bbba77eed1b
youssoufoune <youssef.ettouhami1@gmail.com>
2019-09-23 23:49:30 -0400

    Added files via upload
    First module

---
+++ Workshop/TripTrackers.BackService/TripTrackers.BackService/Controllers/TripsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TripTrackers.BackService.Models;
+
+namespace TripTrackers.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        public TripsController(Models.Repository repository)
+        {
+            _repository = repository; // putting repository in the private field so we can interact with it
+        }
+        private Models.Repository _repository;
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Trip> Get()
+        {
+            return _repository.GetAllTrips();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return _repository.GetTrip(id); 
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Trip trip)
+        {
+            _repository.AddTrip(trip);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trip trip)
+        {
+            _repository.Update(trip);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.RemoveTrip(id);
+        }
+    }
+}
---
+++ Workshop/TripTrackers.BackService/TripTrackers.BackService/Models/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTrackers.BackService.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip> {
+            new Trip{
+                Id = 1,
+                Name= "Hawaii",
+                StartDate = new DateTime(2020,3,1),
+                EndDate = new DateTime(2020,3,15)
+            },
+            new Trip{
+                Id= 2,
+                Name="Morocco",
+                StartDate=new DateTime(2020,8,15),
+                EndDate= new DateTime(2020,9,5)
+            },
+            new Trip{
+                Id= 3,
+                Name="Greece",
+                StartDate = new DateTime(2021,6,15),
+                EndDate = new DateTime(2021,6,25)
+            }
+        };
+        public List<Trip> GetAllTrips() {
+            return MyTrips;
+        }
+        public Trip GetTrip(int id) {
+            return MyTrips.FirstOrDefault(t => t.Id == id);
+        }
+        public void AddTrip(Trip newTrip) {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update(Trip tripToUpdate) {
+            MyTrips.Remove(MyTrips.FirstOrDefault(t => t.Id == tripToUpdate.Id));
+            AddTrip(tripToUpdate);
+        }
+        public void RemoveTrip(int id)
+        {
+            MyTrips.Remove(MyTrips.FirstOrDefault(t => t.Id == id));
+        }
+    }
+}
